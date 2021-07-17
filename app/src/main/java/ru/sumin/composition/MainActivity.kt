71aa5869b827db15f48112f57b0cdb85de404246
a.sumin <a.sumin@cardsmobile.ru>
2021-07-17 11:12:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/sumin/composition/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.sumin.composition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
