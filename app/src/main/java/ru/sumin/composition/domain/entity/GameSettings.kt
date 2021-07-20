9371676bc9fa3d469d269dd0cb636ba3e10b32ef
a.sumin <a.sumin@cardsmobile.ru>
2021-07-20 15:14:22 +0300

    Make GameSettings.kt Serializable

--- app/src/main/java/ru/sumin/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/ru/sumin/composition/domain/entity/GameSettings.kt
@@ -1,8 +1,10 @@
 package ru.sumin.composition.domain.entity
 
+import java.io.Serializable
+
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-)
+): Serializable
